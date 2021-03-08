c966f4b86dc319eb74fc736fef2a37def32cfb70
unknown <anterebol@gmail.com>
2021-03-08 16:51:20 +0300

    fead: sumbit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
+module.exports =  function reverse (arg) {
+    let change = '';
+    arg = Math.abs(arg);
+    arg = arg.toString();
+    for (let i = 0; i < arg.length; i++) {
+      change = arg[i] + change;
+    }
+    return Number(change);
+  }
   
-}
